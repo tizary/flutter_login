549d512e7eada83f505a1ba75bfbee962ef0de84
tizary <tizary@mail.ru>
2024-01-16 23:24:30 +0300

    feat: add search to map, but it is not word, bad apiKey

--- lib/screens/address_page_widget.dart
+++ lib/screens/address_page_widget.dart
@@ -1,24 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/header.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_google_maps_webservices/places.dart';
 
 const currentLocation = LatLng(45.433407932785656, 12.33051294893916);
+const apiKey = 'AIzaSyDWZWwMKwJDS6Fd1cBXIzis1xfOgpreOmg';
 
 class AddressPageWidget extends StatefulWidget {
-  const AddressPageWidget({super.key});
+  const AddressPageWidget({Key? key});
 
   @override
   State<AddressPageWidget> createState() => _AddressPageWidgetState();
 }
 
 class _AddressPageWidgetState extends State<AddressPageWidget> {
+  final _searchController = TextEditingController();
+  GoogleMapController? _mapController;
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  void moveToLocation(LatLng location) {
+    _mapController!.animateCamera(CameraUpdate.newLatLng(location));
+  }
+
+  Future<void> findLocation(String placeName) async {
+    if (placeName.isEmpty) return;
+
+    final places = GoogleMapsPlaces(apiKey: apiKey);
+    var response = await places.searchByText("London");
+    print(response.results);
+    if (response.results.isNotEmpty) {
+      var result = response.results.first;
+      moveToLocation(
+          LatLng(result.geometry!.location.lat, result.geometry!.location.lng));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(pageTitle: 'Address'),
-      body: const GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: currentLocation, zoom: 12.0)),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: _searchController,
+                    textCapitalization: TextCapitalization.words,
+                    decoration: const InputDecoration(
+                      hintText: 'Search',
+                    ),
+                    onChanged: (value) => findLocation(value),
+                  ),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    findLocation(_searchController.text);
+                  },
+                  icon: const Icon(Icons.search))
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition:
+                    CameraPosition(target: currentLocation, zoom: 12.0)),
+          ),
+        ],
+      ),
     );
   }
 }
